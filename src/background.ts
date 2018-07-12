55c9132c4b826c610409b22ee0c6ef43871e69c1
Gao <jogao1983@gmail.com>
2018-07-12 01:23:09 -0700

    move sync logic to background

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,5 @@
+declare var gapi;
+
 
 function polling() {
     console.log('polling');
@@ -6,3 +8,27 @@ function polling() {
 
 polling();
 
+chrome.identity.getAuthToken({ interactive: true }, function (token) {
+    console.log(token);
+    gapi.load('client', function () {
+        gapi.client.setToken({ access_token: token });
+        gapi.client.load('drive', 'v3', loadDrive)
+    })
+});
+
+function loadDrive() {
+    console.log(gapi.client.drive.changes)
+    gapi.client.drive.changes.getStartPageToken({})
+        .execute(function (res) {
+            if (!res.error) {
+                console.log(res.startPageToken)
+                gapi.client.drive.changes.list({ pageToken: res.startPageToken })
+                    .execute(function (res) {
+                        console.log("changes", res)
+                    })
+            }
+            else {
+
+            }
+        })
+}
