c17ee9c8d506f08cf05d52f9fd63c1d5272fbc82
Gao <jogao1983@gmail.com>
2018-07-11 23:00:02 -0700

    working sample to auth and call drive API

--- src/options.ts
+++ src/options.ts
@@ -1,6 +1,8 @@
 import * as moment from 'moment';
 import * as $ from 'jquery';
 
+declare var gapi: any; 
+
 function onload() {
 
 }
@@ -8,7 +10,28 @@ function onload() {
 function signin() {
   chrome.identity.getAuthToken({interactive: true}, function(token) {
     console.log(token);
+    gapi.load('client', function() {
+      gapi.client.setToken({access_token: token});
+      gapi.client.load('drive', 'v3', loadDrive)
+    })
   });
+}
+
+function loadDrive() {
+  console.log(gapi.client.drive.changes)
+  gapi.client.drive.changes.getStartPageToken({})
+    .execute(function (res) {
+      if (!res.error) {
+        console.log(res.startPageToken)
+        gapi.client.drive.changes.list({pageToken: res.startPageToken})
+          .execute(function (res) {
+            console.log("changes", res)
+          })
+      }
+      else {
+
+      }
+    })
 }
 
 
