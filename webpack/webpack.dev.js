0294f002f35cd8fb179b703be97acef28d6e11cb
Gao <jogao1983@gmail.com>
2018-08-02 00:30:49 -0700

    dev build option.html

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,18 +2,19 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'source-map',
-    devServer: {
-        contentBase: './dist',
-        port: 3000,
-        inline: true,
-        hot: true,
-        openPage: 'options.html'
-    },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new CopyWebpackPlugin([
+            {
+                context: 'static/dev',
+                from: 'options.html',
+                to: '../',
+                force: true
+            }
+        ])
     ]
 });
