fbabbc1b79acad71c45786f29e006af9cf54d8fe
Gao <jogao1983@gmail.com>
2018-08-01 23:57:12 -0700

    copy to relative path

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -59,7 +59,7 @@ module.exports = {
             {
                 context: 'static',
                 from: '*',
-                to: 'dist'
+                to: '../'
             }
         ])
     ]
