4214cd0df5bcb806181604b5066f728918b559f1
Gao <jogao1983@gmail.com>
2018-12-04 21:21:58 -0800

    add donation

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -61,13 +61,13 @@ class Options extends React.Component<{}, IOptionsState> {
           <Footer style={{ textAlign: 'center' }}>
             <div>Bookmark Sync @ 120 Studio</div>
             <div>
-              Donate $2 to support us via
-              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" style={{ display: 'inline' }}>
                 <input type="hidden" name="cmd" value="_s-xclick" />
                 <input type="hidden" name="hosted_button_id" value="6R8T89JWF7WFE" />
                 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
                 <img alt="" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
               </form>
+              <span style={{ verticalAlign: 'top' }}> $2 to support us</span>
             </div>
           </Footer>
         </Layout>
