9f149b383f2f11482ef6dae5dc5b27a2a4698ac1
Gao <jogao1983@gmail.com>
2018-12-08 22:44:31 -0800

    integrated with 120home

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -41,8 +41,10 @@ class Options extends React.Component<{}, IOptionsState> {
       <Layout style={{ height: '100vh' }}>
         <Sider trigger={null} collapsible={false} >
           <div className="logo" >
-            <img src='./../bookmark-48.png' />
-            <h1>Drive Mark</h1>
+            <a href='https://120studio.github.io/120home/drivemark.html' target='_blank'>
+              <img src='./../bookmark-48.png' />
+              <h1>Drive Mark</h1>
+            </a>
           </div>
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
             <Menu.Item key="1">
@@ -50,6 +52,14 @@ class Options extends React.Component<{}, IOptionsState> {
               <span>Google Drive</span>
             </Menu.Item>
           </Menu>
+          <div style={{ position: 'absolute', bottom: '20px', marginLeft: '25%' }}>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" style={{ display: 'inline' }}>
+              <input type="hidden" name="cmd" value="_s-xclick" />
+              <input type="hidden" name="hosted_button_id" value="6R8T89JWF7WFE" />
+              <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+              <img alt="" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+            </form>
+          </div>
         </Sider>
         <Layout>
           <Header style={{ background: '#fff', padding: 0 }}>
@@ -61,16 +71,7 @@ class Options extends React.Component<{}, IOptionsState> {
             <GoogleDrivePanel onAccountChange={this.googleAccountChange} />
           </Content>
           <Footer style={{ textAlign: 'center' }}>
-            <div>Drive Mark @ 120 Studio</div>
-            <div>
-              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" style={{ display: 'inline' }}>
-                <input type="hidden" name="cmd" value="_s-xclick" />
-                <input type="hidden" name="hosted_button_id" value="6R8T89JWF7WFE" />
-                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-                <img alt="" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-              </form>
-              <span style={{ verticalAlign: 'top' }}> $2 to support us</span>
-            </div>
+            <div>Drive Mark @ <a href='https://120studio.github.io/120home/'>120 Studio</a></div>
           </Footer>
         </Layout>
       </Layout>
